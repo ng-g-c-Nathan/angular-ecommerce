62c638fcb33cc22824e4440ab0369e38123f1ea6
Nathan <nathanvargasjosue@gmail.com>
2026-03-08 02:33:22 -0600

    BugFix para ver las imagenes

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  apiUrl: '__API_URL__',
+  apiUrl: 'https://spring-ecommerce-api-production.up.railway.app',
   fotosUrl: 'https://spring-ecommerce-api-production.up.railway.app',
   apiKey: '__API_KEY__',
   paypalClientId: '__PAYPAL_KEY__',
